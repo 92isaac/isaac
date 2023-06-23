06d0a51fa65cea9eda7c5a876cd8fdeb3c53ecbf
Sangodare Isaac <sangodareisaac92@gmail.com>
2023-06-23 09:25:03 +0100

    create project with vite, tailwind, typescript, and delete unwanted files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <button className="bg-red-500 px-6 py-2 hover:bg-green-400 rounded-md">Click</button>
+    </>
+  )
+}
+
+export default App
