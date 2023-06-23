b3b323dbff66405d5b2e83156c281e73bc7d6dde
Sangodare Isaac <sangodareisaac92@gmail.com>
2023-06-23 09:58:11 +0100

    add project assets

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,41 @@
+
+const Home = () => {
+  return (
+    <div className="w-full">
+        <div className="flex space-between my-0 mx-auto w-10/12 gap-[50px]">
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
+
+
+
+//   width: 100%;
+
+//   .home__container {
+//     display: flex;
+//     justify-content: space-between;
+//     margin: 0 auto;
+//     margin-top: 90px;
+//     width: 85%;
+//     gap: 50px;
+//   }
+//   @media (max-width: 1440px) {
+//     .home__container {
+//       width: 95%;
+//     }
+//   }
+//   @media (max-width: 1290px) {
+//     .home__container {
+//       gap: 20px !important;
+//     }
+//   }
+//   @media (max-width: 1200px) {
+//     .home__container {
+//       flex-direction: column;
+//     }
+//   }
+// `;
