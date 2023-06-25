b51eb29a3e95342126d1bcdd841da313d92341ef
Sangodare Isaac <sangodareisaac92@gmail.com>
2023-06-25 17:20:57 +0100

    skills components

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,41 +1,25 @@
+import { skills } from "../../utilities/data";
 
 const Skills = () => {
-    return (
-      <div className="w-full">
-          <div className="flex space-between my-0 mx-auto w-10/12 gap-[50px]">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur ea iure cumque, praesentium nihil molestias odio et quibusdam voluptas illum id, veritatis officiis voluptatum quidem. Voluptates doloribus placeat deserunt quis ad sed atque nisi, dolorum nesciunt quo a soluta rem vel labore non magni veritatis maiores itaque praesentium est velit, nobis assumenda? Corporis iure sapiente consequatur fuga harum! Illo voluptate quas officia consequatur labore incidunt aut voluptas blanditiis, quae nulla, ullam itaque consectetur veritatis temporibus libero modi facilis! Earum placeat quaerat provident, impedit hic reiciendis illo assumenda. Vitae eaque eum necessitatibus, recusandae, voluptatum illo esse ullam ipsa alias tempora corporis sapiente? Voluptas voluptates voluptatum dolores, labore qui molestias explicabo ut quia iure quos a, optio blanditiis! Voluptatem velit corporis laborum delectus sed accusamus, possimus cumque explicabo assumenda, hic, sunt amet veniam eos quas minima sequi optio vel nobis inventore? Esse, voluptates libero perferendis aut sint velit est accusantium, rerum excepturi repudiandae explicabo ad laudantium odio sunt laboriosam quaerat aperiam sapiente dolores eius unde qui nihil cupiditate expedita iure. Vitae, necessitatibus. Nam suscipit unde ducimus repudiandae molestias incidunt eos iure officia sed commodi veritatis, officiis quis maiores recusandae debitis vel, sit deserunt nihil expedita laudantium labore quaerat laboriosam consequuntur. Necessitatibus, quas eos, eligendi voluptas, labore a esse atque deleniti temporibus ut maiores assumenda odio! Iste molestiae vero iure, aliquid possimus modi distinctio pariatur, laborum commodi, quisquam architecto nulla quis! Quas officia minima, aperiam neque quam, explicabo ratione nemo magnam laborum, optio doloremque voluptatem ipsum. Modi hic iure rem architecto, numquam eligendi iusto, velit pariatur necessitatibus laudantium, illo vero in. Debitis magnam vero accusantium aliquid </p>
-          </div>
+  return (
+    <div className="w-full">
+      <div className="my-0 mx-auto w-10/12 gap-[50px]">
+        {skills.map((skill)=>(
+            <div className="">
+            <h1 className="text-lg font-bold mb- mt- capitalize ">{skill.name}</h1>
+           <div className="flex items-center gap-4 flex-wrap">{
+            skill.list.map((list)=>(
+                <h2 className="text-sm font-normal px-3 py-2 transform hover:scale-105 transition duration-1000 ease-in-out hover:font-extrabold hover:text-lg border rounded-md">{list}</h2>
+            ))
+            }</div>
+              </div>
+               
+        ))}
       </div>
-    )
-  }
-  
-  export default Skills
-  
-  
-  
-  //   width: 100%;
-  
-  //   .home__container {
-  //     display: flex;
-  //     justify-content: space-between;
-  //     margin: 0 auto;
-  //     margin-top: 90px;
-  //     width: 85%;
-  //     gap: 50px;
-  //   }
-  //   @media (max-width: 1440px) {
-  //     .home__container {
-  //       width: 95%;
-  //     }
-  //   }
-  //   @media (max-width: 1290px) {
-  //     .home__container {
-  //       gap: 20px !important;
-  //     }
-  //   }
-  //   @media (max-width: 1200px) {
-  //     .home__container {
-  //       flex-direction: column;
-  //     }
-  //   }
-  // `;
+    </div>
+  );
+};
+
+export default Skills;
+
+
