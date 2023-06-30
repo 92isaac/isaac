d3849f07d6e8bffc7fbf6c37ef94433ca6de69b4
Sangodare Isaac <sangodareisaac92@gmail.com>
2023-06-30 09:41:58 +0100

    update to mobile footer icon

--- src/components/Footer/MobileFooter.tsx
+++ src/components/Footer/MobileFooter.tsx
@@ -13,11 +13,13 @@ export const MobileFooter = () => {
     <Div className={`-ml-5 ${checked ? 'bg-black text-white' : 'text-black bg-[#e5e5e5]' } flex justify-center items-center md:hidden`}>
          <ul className='flex gap-4 items-center h-16 md:h-10 mr-4'>
             <li className='text-center'>
+              <NavLink to='/' className='text-sm'>
               <GiSkills className='mx-auto my-0 text-lg' />
-              <NavLink to='/' className='text-sm'>Skills</NavLink></li>
+                Skills</NavLink></li>
             <li className='text-center' onClick={()=>handleClick()}>
+              <NavLink to='/projects' className='text-sm'>
               <GoProjectSymlink className='mx-auto my-0 text-lg' />
-              <NavLink to='/projects' className='text-sm'>Project</NavLink></li>
+                Project</NavLink></li>
             <li className='text-center'>
                 <AiOutlineFilePdf className='mx-auto my-0 text-lg' />
               <Link to={'/Sangodare Isaac Oluwatobi Resume.pdf'} target='_blank' rel='noreferrer' className='text-sm'>Resume</Link></li>
